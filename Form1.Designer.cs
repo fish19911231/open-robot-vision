f5904a92ff7274a023abd78a53993b5c577bc11b
kankatee <kankatee@366bd421-e900-d297-837a-5455385f3e55>
2010-08-20 21:20:27 +0000

    Added Hardware Layer Basic FrmMain Layout Controls

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,29 +28,135 @@ namespace OpenRobotVision
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node3");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node4");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Morphologies", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node5");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node6");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Transformations", new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Drawing");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Variables");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Image Filters", new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode6,
+            treeNode7,
+            treeNode8});
             this.PicImg = new System.Windows.Forms.PictureBox();
+            this.TreeFilters = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.captureDevicesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.PicImg)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.MenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // PicImg
             // 
-            this.PicImg.Location = new System.Drawing.Point(13, 13);
+            this.PicImg.Location = new System.Drawing.Point(220, 35);
             this.PicImg.Name = "PicImg";
             this.PicImg.Size = new System.Drawing.Size(100, 50);
             this.PicImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.PicImg.TabIndex = 0;
             this.PicImg.TabStop = false;
             // 
+            // TreeFilters
+            // 
+            this.TreeFilters.Location = new System.Drawing.Point(6, 19);
+            this.TreeFilters.Name = "TreeFilters";
+            treeNode1.Name = "Node3";
+            treeNode1.Text = "Node3";
+            treeNode2.Name = "Node4";
+            treeNode2.Text = "Node4";
+            treeNode3.Name = "Node1";
+            treeNode3.Text = "Morphologies";
+            treeNode4.Name = "Node5";
+            treeNode4.Text = "Node5";
+            treeNode5.Name = "Node6";
+            treeNode5.Text = "Node6";
+            treeNode6.Name = "Node2";
+            treeNode6.Text = "Transformations";
+            treeNode7.Name = "Node7";
+            treeNode7.Text = "Drawing";
+            treeNode8.Name = "Node8";
+            treeNode8.Text = "Variables";
+            treeNode9.Name = "Node0";
+            treeNode9.Text = "Image Filters";
+            this.TreeFilters.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode9});
+            this.TreeFilters.Size = new System.Drawing.Size(178, 308);
+            this.TreeFilters.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TreeFilters);
+            this.groupBox1.Location = new System.Drawing.Point(13, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(190, 333);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter Options";
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.captureDevicesToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(935, 24);
+            this.MenuStrip.TabIndex = 3;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // captureDevicesToolStripMenuItem
+            // 
+            this.captureDevicesToolStripMenuItem.Name = "captureDevicesToolStripMenuItem";
+            this.captureDevicesToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.captureDevicesToolStripMenuItem.Text = "Capture Devices";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(935, 589);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.PicImg);
+            this.Controls.Add(this.MenuStrip);
+            this.MainMenuStrip = this.MenuStrip;
             this.Name = "FrmMain";
             this.Text = "Open Robot Vision";
             this.Load += new System.EventHandler(this.FrmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.PicImg)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,6 +165,13 @@ namespace OpenRobotVision
         #endregion
 
         private System.Windows.Forms.PictureBox PicImg;
+        private System.Windows.Forms.TreeView TreeFilters;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem captureDevicesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
